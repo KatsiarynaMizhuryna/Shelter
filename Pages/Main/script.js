0b00c804a217e2a6ac5e318ccd8101be8dae0dcb
Kate <ekaterina21051988@yandex.by>
2023-03-28 19:51:07 +0300

    fix: changed responsive styles width 767px

--- Pages/Main/script.js
+++ Pages/Main/script.js
@@ -15,23 +15,31 @@ e.preventDefault()
     })
 });
 
+const logo = document.querySelector(".logo")
+const logo2 = logo.cloneNode(true)
+console.log(logo2)
 
  /* ----------------------burger for main page--------------------------- */
 const burgerMenu = document.querySelector(".burger-menu")
 const navigation = document.querySelector(".navigation")
+const blackout = document.querySelector(".burger-menu_blackout")
 
 burgerMenu.addEventListener("click", ()=>{
+    navigation.appendChild(logo2)
+    logo2.classList.add("vis")
     document.body.classList.toggle("lock")
     burgerMenu.classList.toggle("active-burger")
     navigation.classList.toggle("active-burger")
+    blackout.classList.toggle("blackout-active")
 })
 
 menuLinks.forEach(menuLink => {
     menuLink.addEventListener("click", (e)=> {
-if (burgerMenu.classList.contains("active-burger")) {
+        if (burgerMenu.classList.contains("active-burger")) {
     document.body.classList.remove("lock")
     burgerMenu.classList.remove("active-burger")
     navigation.classList.remove("active-burger")
-}
+    blackout.classList.remove("blackout-active")
+        }
     })
 });
