be58987a302d0f2041b2bb6ebd10730a3a6d93c7
Kate <ekaterina21051988@yandex.by>
2023-04-03 16:07:09 +0300

    feat: added styles for popup

--- Pages/info.js
+++ Pages/info.js
@@ -1,7 +1,7 @@
 const petsInfo = [
     {
       "name": "Jennifer",
-      "img": "./Assets/Images/main_page/pets-katrine.png",
+      "img": "./Assets/Images/main_page/pets-jennifer.png",
       "type": "Dog",
       "breed": "Labrador",
       "description": "Jennifer is a sweet 2 months old Labrador that is patiently waiting to find a new forever home. This girl really enjoys being able to go outside to run and play, but won't hesitate to play up a storm in the house if she has all of her favorite toys.",
